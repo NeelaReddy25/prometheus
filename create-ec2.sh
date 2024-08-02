b2bacfeaab7881a27bb068bc6148acd9ccd7e5a1
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-02 18:46:08 +0530

    good effects

--- create-ec2.sh
+++ create-ec2.sh
@@ -17,15 +17,6 @@ for name in ${instances[@]}; do
     instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type $instance_type --security-group-ids sg-0cd5626364cf1e071 --subnet-id subnet-045b66b79d1f5cc3f --query 'Instances[0].InstanceId' --output text)
     echo "Instance created for: $name"
 
-    existing_instance_id=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$existing_instance_name" --query 'Reservations[0].Instances[0].InstanceId' --output text)
-
-    if [ "$existing_instance_id" == "None" ]; then
-        echo "Error: Instance with name $existing_instance_name not found."
-        exit 1
-    fi
-
-    aws ec2 wait instance-running --instance-ids $existing_instance_id
-
     aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
 
     if [ $name == "node-1" ] || [ $name == "node-2" ]
@@ -42,14 +33,7 @@ for name in ${instances[@]}; do
         private_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text)
         ip_to_use=$private_ip
     fi
-    public_ip=$(aws ec2 describe-instances --instance-ids $existing_instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
-    if [ "$public_ip" == "None" ]; then
-        echo "Error: Public IP not found for instance $existing_instance_name."
-        exit 1
-    fi
-
-    echo "Public IP of instance $existing_instance_name is $public_ip"
     echo "Creating R53 record for: $name"
     aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch '
     {
@@ -66,7 +50,24 @@ for name in ${instances[@]}; do
         }
         }]
     }'
+    existing_instance_id=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$existing_instance_name" --query 'Reservations[0].Instances[0].InstanceId' --output text)
 
+    if [ "$existing_instance_id" == "None" ]; then
+        echo "Error: Instance with name $existing_instance_name not found."
+        exit 1
+    fi
+    
+    aws ec2 wait instance-running --instance-ids $existing_instance_id
+
+    public_ip=$(aws ec2 describe-instances --instance-ids $existing_instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
+    if [ "$public_ip" == "None" ]; then
+        echo "Error: Public IP not found for instance $existing_instance_name."
+        exit 1
+    fi
+
+    echo "Public IP of instance $existing_instance_name is $public_ip"
+    
     for record_name in "${record_names[@]}"; do
         echo "Creating DNS record for: $record_name.$domain_name with IP $public_ip"
         aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch '
